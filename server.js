609bb32dc7a9672012b1049bb8df43068c9b4fd0
Jacob Goskha <jacobgoskha@gmail.com>
2019-12-17 10:49:49 -0500

    MongoDB connection

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./db/db');
 
 const app = express();
 
+// ENV
+require('dotenv').config();
+
+// DB
+connectDB();
+
 app.get('/', (req, res) => {
-  res.send('API Running');
+  res.send('API');
 });
 
 const PORT = process.env.PORT || 5000;
