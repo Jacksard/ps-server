8158d888cdd51930532752f31abecf6d2c117565
Jacob Goskha <jacobgoskha@gmail.com>
2019-12-18 15:26:16 -0500

    profile model + get users current profile

---
+++ models/Profile.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  website: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      fieldOfStudy: {
+        type: String,
+        require: true
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    facebook: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
